d11c6edcf19d8e6e73c3c301cb330b6290aaa7a8
Aristarkhus <aristarkhus.dwiki@ti.ukdw.ac.id>
2018-09-24 12:07:19 +0700

    s

--- index.php
+++ index.php
@@ -1,67 +1,14 @@
 <!DOCTYPE html>
 <html>
 <head>
-	<!-- Required meta tags -->
-        <meta charset="utf-8">
-        <meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no">
-        <link rel="icon" href="img/tukuo.png" type="image/png">
-
-        <title>TUKUO</title>
-        <!-- Bootstrap CSS -->
-        <link rel="stylesheet" href="css/bootstrap.css">
-        <link rel="stylesheet" href="vendors/linericon/style.css">
-        <link rel="stylesheet" href="css/font-awesome.min.css">
-        <link rel="stylesheet" href="vendors/owl-carousel/owl.carousel.min.css">
-        <link rel="stylesheet" href="vendors/lightbox/simpleLightbox.css">
-        <link rel="stylesheet" href="vendors/nice-select/css/nice-select.css">
-        <link rel="stylesheet" href="vendors/animate-css/animate.css">
-        <link rel="stylesheet" href="vendors/jquery-ui/jquery-ui.css"> 
-        <!-- main css -->
-        <link rel="stylesheet" href="css/style.css">
-        <link rel="stylesheet" href="css/responsive.css">
+	<?php 
+		require "style.php";
+	 ?>
 </head>
 <body>
-	<!--================Header Menu Area =================-->
-        <header class="header_area">
-           	<div class="top_menu row m0">
-           		<div class="container">
-					<div class="float-left">
-						<a href="#">tukuo@gmail.com</a>
-					</div>
-					<div class="float-right">
-						<ul class="header_social">
-							<li><a href="https://web.facebook.com/abed.petraprasetya"><i class="fa fa-facebook"></i></a></li>
-							<li><a href="https://twitter.com/AbednegoPetra"><i class="fa fa-twitter"></i></a></li>
-							<li><a href="https://www.instagram.com/bebedsky_/"><i class="fa fa-instagram"></i></a></li>
-						</ul>
-					</div>
-           		</div>	
-           	</div>	
-            <div class="main_menu">
-            	<nav class="navbar navbar-expand-lg navbar-light main_box">
-					<div class="container">
-						<!-- Brand and toggle get grouped for better mobile display -->
-						<a class="navbar-brand logo_h" href="index.php"><img src="img/logo.png" alt=""></a>
-						<button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="Toggle navigation">
-							<span class="icon-bar"></span>
-							<span class="icon-bar"></span>
-							<span class="icon-bar"></span>
-						</button>
-						<!-- Collect the nav links, forms, and other content for toggling -->
-						<div class="collapse navbar-collapse offset" id="navbarSupportedContent">
-							<ul class="nav navbar-nav menu_nav ml-auto">
-								<li class="nav-item active"><a class="nav-link" href="index.php">Home</a></li> 
-								<li class="nav-item"><a class="nav-link" href="#">About Us</a></li>
-							</ul>
-							<ul class="nav navbar-nav navbar-right">
-								<li class="nav-item"><a class="nav-link" href="#">Login</a></li>
-							</ul>
-						</div> 
-					</div>
-            	</nav>
-            </div>
-        </header>
-        <!--================Header Menu Area =================-->
+		<?php 
+			require "header.php";
+		 ?>
 
         <!--================Home Banner Area =================-->
         <section class="home_banner_area">
@@ -234,6 +181,7 @@
     	}
     ?>
 
+
     <!--================Clients Logo Area =================-->
         <section class="clients_logo_area">
         	<div class="container">
@@ -313,39 +261,10 @@
         </section>
         <!--================End Latest Product Area =================-->
 
-
-        <!--================ start footer Area  =================-->	
-        <footer class="footer-area">
-            <div class="container">
-                <div class="row footer-bottom d-flex justify-content-between align-items-center">
-                    <div class="col-lg-12 footer-text text-center">
-                        <h6 class="footer_title">About Us</h6>
-                        <p>Rasah kakean fafifu wes langsung gaske wae.</p>
-                    </div>
-                </div>						
-                <div class="row footer-bottom d-flex justify-content-between align-items-center">
-                    <p class="col-lg-12 footer-text text-center"> Copyright &copy;<script>document.write(new Date().getFullYear());</script> All rights reserved</p>
-                </div>
-            </div>
-        </footer>
-		<!--================ End footer Area  =================-->
-
-
-        <script src="js/jquery-3.2.1.min.js"></script>
-        <script src="js/popper.js"></script>
-        <script src="js/bootstrap.min.js"></script>
-        <script src="js/stellar.js"></script>
-        <script src="vendors/lightbox/simpleLightbox.min.js"></script>
-        <script src="vendors/nice-select/js/jquery.nice-select.min.js"></script>
-        <script src="vendors/isotope/imagesloaded.pkgd.min.js"></script>
-        <script src="vendors/isotope/isotope-min.js"></script>
-        <script src="vendors/owl-carousel/owl.carousel.min.js"></script>
-        <script src="js/jquery.ajaxchimp.min.js"></script>
-        <script src="vendors/counter-up/jquery.waypoints.min.js"></script>
-        <script src="vendors/flipclock/timer.js"></script>
-        <script src="vendors/counter-up/jquery.counterup.js"></script>
-        <script src="js/mail-script.js"></script>
-        <script src="js/theme.js"></script>
+       <?php 
+       		require "footer.php";
+       		require "java.php";
+        ?>
 
 	
 	</body>
